29fd5b060ec16338ce73c865dcca6af5ee23e9f4
cuongha12 <103917651+cuongha12@users.noreply.github.com>
2024-01-08 17:19:12 +0700

    Update worker.js

--- src/worker/worker.js
+++ src/worker/worker.js
@@ -6,8 +6,6 @@ import { parentPort, workerData } from 'worker_threads';
 async function run() {
   const app = await NestFactory.createApplicationContext(AppModule);
   const appService = app.get(AppService);
-  console.log('Worker thread got data:', workerData);
-  appService.blocking(workerData.cpuTimeMs);
   parentPort.postMessage(workerData);
 }
 
